21c495bd8efb2851dec12df1c0a7ebf26597a6f4
David Girault <david@dhgirault.fr>
2025-11-29 19:05:44 +0100

    [Keyboard] Add OLED init and user functions

--- keyboards/resplit66/rev1u/config.h
+++ keyboards/resplit66/rev1u/config.h
@@ -12,3 +12,24 @@
 /* For RGB matrix using qmk ws2812 driver */
 #define WS2812_SPI_DRIVER SPID2
 #define WS2812_SPI_DIVISOR 32
+
+/* For OLED using qmk default driver */
+#define I2C_DRIVER I2CD2
+#define I2C1_SCL_PIN B10
+#define I2C1_SDA_PIN B11
+
+/* From STM32CubeMX project used for Test FW:
+I2C1.I2C_Speed_Mode=I2C_Fast
+I2C1.IPParameters=I2C_Speed_Mode,Timing
+I2C1.Timing=0x00702991
+I2C2.IPParameters=Timing
+I2C2.Timing=0x10909CEC
+*/
+/* Use FastMode */
+#define I2C1_TIMINGR_PRESC  0x00u // <<28
+#define I2C1_TIMINGR_SCLDEL 0x07u // <<20
+#define I2C1_TIMINGR_SDADEL 0x00u // <<16
+#define I2C1_TIMINGR_SCLH   0x29u // << 8
+#define I2C1_TIMINGR_SCLL   0x91u // << 0
+
+#define OLED_DISPLAY_128X64
